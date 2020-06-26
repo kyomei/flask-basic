6b10f8c1a507740df72de20646a62cdbb70c7f18
Rafael pena <rafael.pena@visie.com.br>
2020-06-26 20:43:57 -0300

    Criando rotas simples no flask

---
+++ app.py
@@ -0,0 +1,22 @@
+# Aula 01 - Instalando o Flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello world"
+
+def teste():
+    return "<p>Testando 1</p>"
+
+def teste2():
+    return "<h1>Testando 2</h1>"
+
+app.add_url_rule("/teste", "teste", teste)
+app.add_url_rule("/teste-2", "teste2", teste2)
+
+if __name__ == '__main__':
+    app.run(debug=True, port="3000")
